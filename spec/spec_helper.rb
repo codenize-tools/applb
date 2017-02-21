fc63556acb0fb44c1bb9719ed3f7b74296742486
shinya-watanabe <shinya-watanabe@cookpad.com>
2017-02-21 19:23:01 +0900

    Add interval for spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,11 @@ require 'pry'
 
 aws_config_path = Pathname.new(File.expand_path('../', __FILE__)).join('aws_config.yml')
 AWS_CONFIG = Hashie::Mash.new(YAML.load_file(aws_config_path))
+
+TEST_INTERVAL = ENV['TEST_INTERVAL'].to_i
+
+RSpec.configure do |config|
+  config.before(:each) do
+    sleep TEST_INTERVAL
+  end
+end
