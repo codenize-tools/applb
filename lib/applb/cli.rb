bd5a5ad71b13811dfe565228904b6348cd55bf24
shinya-watanabe <shinya-watanabe@cookpad.com>
2017-02-22 16:33:49 +0900

    Add help and remove tabs for template

--- lib/applb/cli.rb
+++ lib/applb/cli.rb
@@ -10,8 +10,8 @@ module Applb
 
     def initialize(argv)
       @argv = argv.dup
-      @help = false
-      @filepath = 'ALBFile'
+      @help = argv.empty?
+      @filepath = 'ALBfile'
       @options = {
         color: true,
         includes: [],
@@ -21,7 +21,9 @@ module Applb
     end
 
     def run
-      if @apply
+      if @help
+        puts parser.help
+      elsif @apply
         Apply.new(@filepath, @options).run
       elsif @export
         Export.new(@filepath, @options).run
@@ -32,26 +34,19 @@ module Applb
 
     def parser
       @parser ||= OptionParser.new do |opts|
-        opts.banner = 'applb'
         opts.version = VERSION
         opts.on('-h', '--help', 'Show help') { @help = true }
         opts.on('-v', '--debug', 'Show debug log') { Applb.logger.level = Logger::DEBUG }
         opts.on('-a', '--apply', 'apply DSL') { @apply = true }
         opts.on('-e', '--export', 'export to DSL') { @export = true }
         opts.on('-n', '--dry-run', 'dry run') { @options[:dry_run] = true }
-        opts.on('-f', '--file=FILE', 'use selected DSL file') { |v| @filepath = v }
-        opts.on('-s', '--split', 'split export DSL file') { @options[:split] = true }
-        opts.on('',   '--split-more', 'split 1 file per load balancer') { @options[:split_more] = true }
+        opts.on('-f', '--file FILE', 'use selected DSL file') { |v| @filepath = v }
+        opts.on('-s', '--split', 'split export DSL file to 1 per VPC') { @options[:split] = true }
+        opts.on('',   '--split-more', 'split export DSL file to 1 per load balancer') { @options[:split_more] = true }
         opts.on('',   '--no-color', 'no color') { @options[:color] = false }
-        opts.on('-i', '--include_names=NAMES', 'include names', Array) { |v| @options[:includes] = v }
-        opts.on('',   '--include_target_groups=NAMES', 'include target_groups', Array) { |v| @options[:include_target_groups] = v}
-        opts.on('-x', '--exclude_names=NAMES', 'exclude names', Array) do |v|
+        opts.on('-i', '--include_names NAMES', 'include ELB v2(ALB) names', Array) { |v| @options[:includes] = v }
+        opts.on('-x', '--exclude_names NAMES', 'exclude ELB v2(ALB) names by regex', Array) do |v|
           @options[:excludes] = v.map! do |name|
-            name =~ /\A\/(.*)\/\z/ ? Regexp.new($1) : Regexp.new("\A#{Regexp.escape(name)}\z")
-          end
-        end
-        opts.on('',   '--exclude_target_groups=NAMES', 'exclude target_groups', Array) do |v|
-          @options[:exclude_target_groups] = v.map! do |name|
             name =~ /\A\/(.*)\/\z/ ? Regexp.new($1) : Regexp.new("\A#{Regexp.escape(name)}\z")
           end
         end
