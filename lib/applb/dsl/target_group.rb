b8490d328f6cce01eb63afc9c9de4dd3802caaeb
kobayashi-hirotake <hkobayash0220@gmail.com>
2017-05-12 13:33:06 +0900

    register or deregister instances with target_group

--- lib/applb/dsl/target_group.rb
+++ lib/applb/dsl/target_group.rb
@@ -11,7 +11,7 @@ module Applb
               ATTRIBUTES = %i/
                 name protocol port vpc_id health_check_protocol health_check_port health_check_path
                 health_check_interval_seconds health_check_timeout_seconds healthy_threshold_count
-                unhealthy_threshold_count matcher
+                unhealthy_threshold_count matcher instances
               /
 
               attr_accessor *ATTRIBUTES
@@ -27,6 +27,10 @@ module Applb
 
               def aws(aws_tg)
                 @aws_tg = aws_tg
+                @aws_instances = client.
+                  describe_target_health(target_group_arn: @aws_tg.target_group_arn).
+                  target_health_descriptions.reject { |description| description.target_health.state == "draining" }.
+                  map(&:target).map(&:id).sort
                 self
               end
 
@@ -45,19 +49,23 @@ module Applb
                 Applb.logger.info("<diff>\n#{Applb::Utils.diff(aws_hash, dsl_hash, color: @options[:color])}")
                 return if @options[:dry_run]
 
+                modify_instances
                 client.modify_target_group(modify_option).target_groups.first
               end
 
               private
 
               def create_option
-                to_h
+                hash = to_h
+                hash.delete(:instances)
+                hash
               end
 
               UNMODIFIABLE_ATTRIBUTES = %i/name port protocol vpc_id/
               def modify_option
-                options = to_h.
-                  merge(target_group_arn: @aws_tg.target_group_arn).
+                hash = to_h
+                hash.delete(:instances)
+                hash.merge(target_group_arn: @aws_tg.target_group_arn).
                   reject! { |k, v| UNMODIFIABLE_ATTRIBUTES.include?(k) }
               end
 
@@ -70,9 +78,28 @@ module Applb
               def to_diff_h_aws
                 hash = @aws_tg.to_h
                 hash[:name] = hash.delete(:target_group_name)
+                hash[:instances] = @aws_instances
                 hash.delete(:target_group_arn)
                 hash.delete(:load_balancer_arns)
                 Hash[hash.sort]
+              end
+
+              def modify_instances
+                return if instances == @aws_instances
+
+                register_targets = (instances - @aws_instances).map do |instance|
+                  {id: instance}
+                end
+                if !register_targets.empty?
+                  client.register_targets(target_group_arn: @aws_tg.target_group_arn, targets: register_targets)
+                end
+
+                deregister_targets = (@aws_instances - instances).map do |instance|
+                  {id: instance}
+                end
+                if !deregister_targets.empty?
+                  client.deregister_targets(target_group_arn: @aws_tg.target_group_arn, targets: deregister_targets)
+                end
               end
 
               def client
@@ -85,6 +112,7 @@ module Applb
               @lb_name = lb_name
               @result = Result.new(@context)
               @result.name = name
+              @result.instances = []
 
               instance_eval(&block)
             end
@@ -146,6 +174,10 @@ module Applb
 
             def matcher(http_code:)
               @result.matcher = { http_code: http_code }
+            end
+
+            def instances(*instances)
+              @result.instances = instances.sort
             end
           end
         end
